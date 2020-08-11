d8ec8c933c8da0fb22473bae4fe8c04b9f66070f
Akira <akira.brand@yahoo.com>
2020-08-11 04:25:53 +0000

    add fakeAjax, have lots of questions

--- index.js
+++ index.js
@@ -3,31 +3,50 @@ class Bookshelf {
 		this.favoriteBooks = [];
 	}
 
-  addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		this.favoriteBooks.push(bookName);
-	}
+    addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+    printFavoriteBooks() {
+    console.log(`Favorite Books: ${this.favoriteBooks.length}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(String(bookName));
+    }
+  }
 }
-  printFavoriteBooks() {
-	console.log(`Favorite Books: ${this.favoriteBooks.length}`);
-	for (let bookName of this.favoriteBooks) {
-		console.log(String(bookName));
-	}
+
+function loadBooks(bookshelf) {
+	fakeAjax(BOOK_API,function onBooks(bookNames){
+		for (let bookName of bookNames) {
+			bookshelf.addFavoriteBook(bookName);
+		}
+		bookshelf.printFavoriteBooks();
+	});
 }
-	// and `printFavoriteBooks()`
+
+var BOOK_API = "https://some.url/api";
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
+
+// NOTE: don't modify this function at all
+function fakeAjax(url,cb) {
+	setTimeout(function fakeLoadingDelay(){
+		cb([
+			"A Song of Ice and Fire",
+			"The Great Gatsby",
+			"Crime & Punishment",
+			"Great Expectations",
+			"You Don't Know JS"
+		]);
+	},500);
 }
+
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
 
 
 var bookshelf = new Bookshelf();
-
 bookshelf.addFavoriteBook("The Shining");
 bookshelf.printFavoriteBooks();
-
-
-//function printFavoriteBooks() {
-// 	console.log(`Favorite Books: ${favoriteBooks.length}`);
-// 	for (let bookName of favoriteBooks) {
-// 		console.log(String(bookName));
-// 	}
-// }
-
+loadBooks(myBooks);
