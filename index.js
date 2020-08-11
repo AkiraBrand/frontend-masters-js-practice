cc84da41dbe3c433b4f4a10103e09cc83afb06a2
Akira <akira.brand@yahoo.com>
2020-08-11 04:14:59 +0000

    adds addFavoriteBook and printFavoriteBook to class Bookshelf

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
+class Bookshelf {
+	constructor() {
+		this.favoriteBooks = [];
+	}
+
+  addFavoriteBook(bookName) {
+	if (!bookName.includes("Great")) {
+		this.favoriteBooks.push(bookName);
+	}
+}
+  printFavoriteBooks() {
+	console.log(`Favorite Books: ${this.favoriteBooks.length}`);
+	for (let bookName of this.favoriteBooks) {
 		console.log(String(bookName));
 	}
 }
+	// and `printFavoriteBooks()`
+}
 
-favoriteBooks = ["A Song of Ice and Fire", 1, null];
 
-printFavoriteBooks();
+var bookshelf = new Bookshelf();
+
+bookshelf.addFavoriteBook("The Shining");
+bookshelf.printFavoriteBooks();
+
+
+//function printFavoriteBooks() {
+// 	console.log(`Favorite Books: ${favoriteBooks.length}`);
+// 	for (let bookName of favoriteBooks) {
+// 		console.log(String(bookName));
+// 	}
+// }
+
