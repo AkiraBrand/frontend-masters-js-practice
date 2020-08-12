b5eb5a6ad7bfd5bc1f66433c296205ab04c2683c
Akira <akira.brand@yahoo.com>
2020-08-12 01:49:50 +0000

    creates loadBooks function w/ fake ajax call

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ class Bookshelf {
 ///this is OUTSIDE THE BOOKSHELF CLASS
 
 function loadBooks(bookshelf) {
+  //inline function here
+  //passed an array-> bookNames
+  fakeAjax(BOOK_API,function onBooks(bookNames){
+    //loop array 
+    for (let bookName of bookNames) {
+      //use the bookshelf object, call the addFavoriteBook method on it, pass it the bookName inside the bookNames array 
+      bookshelf.addFavoriteBook(bookName);
+    }
+    // call the printfavebooks method on the bookshelf object
+    bookshelf.printFavoriteBooks();
+  });
 }
 
 var BOOK_API = "https://some.url/api";
@@ -37,6 +48,4 @@ function fakeAjax(url,cb) {
 }
 
 var bookshelf = new Bookshelf();
-bookshelf.addFavoriteBook("The Shining");
-bookshelf.printFavoriteBooks();
-loadBooks(myBooks);
+loadBooks(bookshelf);
